51d1035bdf6d55530096c4e191146d56e5c13673
dvdbyte <dvdbyte1@gmail.com>
2024-07-20 20:39:00 +0100

    describeAge

--- describeAge.js
+++ describeAge.js
@@ -19,3 +19,5 @@ Whatever you do, do not change what the function does. Good luck :)
 
 describeAge=a=>"You're a(n) "+(a<=12?'kid':a>=13&&a<=17?'teenager':a>=18&&a<=64?'adult':'elderly')
 
+
+// Touched for 2024
