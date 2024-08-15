0f21a51f2ff2eba155c85f2f4db8e0c38d7482d6
dvdbyte <dvdbyte1@gmail.com>
2024-08-15 19:17:00 +0100

    learningToHula

--- learningToHula.js
+++ learningToHula.js
@@ -13,3 +13,5 @@ Write a program where Alex can input (n) how many times the hoop goes round and 
 function hoopCount (n) {
 return n<10?"Keep at it until you get it":"Great, now move on to tricks"
 }
+
+// Touched for 2024
