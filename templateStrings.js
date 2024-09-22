ea15b090a2b94e6817171844e68a9349ed99035b
dvdbyte <dvdbyte1@gmail.com>
2024-09-22 21:35:00 +0100

    templateStrings

--- templateStrings.js
+++ templateStrings.js
@@ -11,3 +11,5 @@ You must output a string containing the two strings with the word ```' are '```
 var TempleStrings = function(obj, feature) {
   return `${obj} are ${feature}`
 }
+
+// Touched for 2024
