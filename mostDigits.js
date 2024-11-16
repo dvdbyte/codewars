c85f6c0a23f92bad23e96a466506ad25a7c1e178
dvdbyte <dvdbyte1@gmail.com>
2024-11-16 20:13:00 +0100

    mostDigits

--- mostDigits.js
+++ mostDigits.js
@@ -9,3 +9,5 @@ If two numbers in the argument array have the same number of digits, return the 
 function findLongest(array){
 return +array.map(String).sort((a,b)=>b.length-a.length)[0]
 }
+
+// Touched for 2024
