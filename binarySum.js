ae65cc6a73dad1824064dcbe2a55f8e8d3bac53c
dvdbyte <dvdbyte1@gmail.com>
2025-04-05 22:54:00 +0100

    binarySum

--- binarySum.js
+++ binarySum.js
@@ -19,4 +19,4 @@ let arr2bin = arr => arr.reduce((x,y)=>x+(typeof y=="number"?y:0),0).toString(2)
 
 let arr2bin = (arr) => arr.filter(x => typeof x ==='number').reduce((a, b) => a + b,0).toString(2)
 
-// Touched for 2024
+// Touched for 2025/2026
