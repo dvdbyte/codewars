d1530a58067efb72796c9e138a2eb2ecd5fac556
dvdbyte <dvdbyte1@gmail.com>
2025-06-25 20:23:00 +0100

    arrayType

--- arrayType.js
+++ arrayType.js
@@ -22,3 +22,5 @@ blackAndWhite([5,12]) should return "It's a white array"
 function blackAndWhite(arr){
  return !(Array.isArray(arr))?"It's a fake array":arr.indexOf(5)!==-1&&arr.indexOf(13)!==-1?"It's a black array":"It's a white array"
 }
+
+// Touched for 2025/2026
