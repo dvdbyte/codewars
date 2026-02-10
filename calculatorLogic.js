744a5ed98624709265c5e9bee8261250d324805a
dvdbyte <dvdbyte1@gmail.com>
2026-02-10 18:46:00 +0100

    calculatorLogic

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -30,3 +30,5 @@ function calculator(a,b,sign){
 if (typeof(a)!=='number'||typeof(b)!=='number'){return "unknown value"}
 if(sign==='*'){return a*b}else if(sign==='/'){return a/b}else if(sign==='+'){return a+b}else if(sign==='-') {return a-b} else {return 'unknown value'}
 }
+
+// Touched for 2025/2026
